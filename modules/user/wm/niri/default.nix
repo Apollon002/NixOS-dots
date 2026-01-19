c380f2ad564251021c6f19a9cba3483cce39bd7b
Apollon002 <kontakt@jannikzenker.de>
2026-01-19 22:57:39 +0100

    Included logic that imports niri configs to a diffrent path needed by dms

--- modules/user/wm/niri/default.nix
+++ modules/user/wm/niri/default.nix
@@ -1,11 +1,12 @@
 { config, lib, ... }:
 let
   isEnabled = builtins.elem "niri" (config.userSettings.wm.enable or [ ]);
-  dmsEnabled = config.ui.dms.enable;
+  dmsEnabled = config.userSettings.ui.dms.enable;
+  # DankMaterialShell needs a diffrent path for imports
   path = if dmsEnabled then "niri/dms" else "niri";
 in
 {
-  xdg.configFile = lib.mkIf (isEnabled && !dmsEnabled) {
+  xdg.configFile = lib.mkIf isEnabled {
     # Alle Dateien/Ordner aus repo/config/niri nach ~/.config/niri/
     "niri".source = ./config;
     "niri".target = path;
