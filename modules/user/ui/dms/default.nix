8d8473094d2d41f32622b79e10d1e28cd2ad931b
Apollon002 <kontakt@jannikzenker.de>
2026-01-20 00:08:57 +0100

    Added dms and colors from dms for dynamic theming for niri

--- modules/user/ui/dms/default.nix
+++ modules/user/ui/dms/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   lib,
   inputs,
   ...
@@ -8,11 +9,27 @@ let
   cfg = config.userSettings.ui.dms;
 in
 {
-  imports = [ inputs.dms.homeModules.dank-material-shell ];
+  imports = [
+    inputs.dms.homeModules.dank-material-shell
+  ];
 
   options.userSettings.ui.dms.enable = lib.mkEnableOption "Enable DankMaterialShell";
 
   config = lib.mkIf (cfg.enable) {
-    # programs.dank-material-shell.enable = true;
+    home.packages = with pkgs; [
+      matugen
+      i2c-tools
+      cava
+    ];
+    programs.dank-material-shell = {
+      enable = true;
+
+      systemd = {
+        enable = true;
+        restartIfChanged = true;
+      };
+      # Features
+      enableDynamicTheming = true;
+    };
   };
 }
