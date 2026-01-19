ab4f25be7b7c1357de5495a1b0d70b4d40c407c1
Apollon002 <kontakt@jannikzenker.de>
2026-01-20 00:20:28 +0100

    Added new logic that passes diffrent versions of config.kdl entry point for niri if dms is active

--- modules/user/wm/niri/default.nix
+++ modules/user/wm/niri/default.nix
@@ -1,11 +1,15 @@
 { config, lib, ... }:
 let
   isEnabled = builtins.elem "niri" (config.userSettings.wm.enable or [ ]);
+  dmsEnabled = config.userSettings.ui.dms.enable;
 in
 {
   xdg.configFile = lib.mkIf isEnabled {
     # Alle Dateien/Ordner aus repo/config/niri nach ~/.config/niri/
     "niri".source = ./config;
     "niri".recursive = true;
+
+    "niri/config.kdl".text =
+      builtins.readFile ./config.kdl + "\n" + lib.optionalString dmsEnabled ''include "dms/colors.kdl"'';
   };
 }
