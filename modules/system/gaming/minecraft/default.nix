dfb19499ed44479fd3839298b2178ae502ad1778
Apollon002 <kontakt@jannikzenker.de>
2026-01-20 21:26:51 +0100

    Fix: Fixed a typo that prevented prism from installing

--- modules/system/gaming/minecraft/default.nix
+++ modules/system/gaming/minecraft/default.nix
@@ -11,7 +11,7 @@ in
   options.systemSettings.gaming.minecraft.enable =
     lib.mkEnableOption "Install Minecraft (Prism launcher)";
 
-  config = lib.mfIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       prismlauncher
       jdk21
