b0df5bf92c7c137fffccc3ec0f822dc2602e5c9c
Apollon002 <kontakt@jannikzenker.de>
2026-01-20 23:15:04 +0100

    Added modularity to userConfig. - Category: Terminals - Category: Programs

---
+++ modules/user/terminals/default.nix
@@ -0,0 +1,12 @@
+# Imports all terminal modules
+{ ... }:
+let
+  entries = builtins.readDir ./.;
+
+  subdirs = builtins.filter (name: entries.${name} == "directory") (builtins.attrNames entries);
+
+  importsFromSubdirs = map (name: ./. + "/${name}/default.nix") subdirs;
+in
+{
+  imports = importsFromSubdirs;
+}
