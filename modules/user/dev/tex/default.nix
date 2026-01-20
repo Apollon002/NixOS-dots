5c109932c63ea2d97a450ae9be1f7d8d4cb6c7c8
Apollon002 <kontakt@jannikzenker.de>
2026-01-20 18:20:21 +0100

    Added all tex packages to latex install

--- modules/user/dev/tex/default.nix
+++ modules/user/dev/tex/default.nix
@@ -5,74 +5,14 @@
   ...
 }:
 let
-  cfg = config.userSettings.latex;
+  cfg = config.userSettings.dev.latex;
 in
 {
-  options.userSettings.latex.enable = lib.mkEnableOption "Install Latex (full)";
+  options.userSettings.dev.latex.enable = lib.mkEnableOption "Install Latex (full)";
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
-      (texlive.combine {
-        inherit (texlive)
-          # Base
-          scheme-small
-          latexmk # Perl script for automated build process
-
-          # LuaLatex / Fonts
-          fontspec
-          luaotfload
-          lualatex-math
-
-          # Math packages
-          amsmath
-          amsfonts
-          amssymb
-          mathtools
-          physics
-
-          # Layout & colors
-          geometry
-          xcolor
-          setspace
-          fancyhdr
-          microtype
-
-          # Graphics and tables
-          graphicx
-          booktabs
-          longtable
-          array
-          multirow
-          tabularx
-          float
-          caption
-          subcaption
-
-          # References & links
-          hyperref
-          cleverref
-
-          # Language & quotes
-          babel
-          csquotes
-
-          # Bibliography
-          biblatex
-          biber
-
-          # Beamer
-          beamer
-
-          # Code
-          listings
-          minted
-
-          # Utilities
-          etoolbox
-          xkeyval
-          iftex
-          ;
-      })
+      texlive.combined.scheme-full
     ];
   };
 }
