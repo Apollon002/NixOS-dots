7531941cf28d67988704c2d1074c3a7f2c7da6ed
Apollon002 <kontakt@jannikzenker.de>
2026-01-21 02:11:14 +0100

    Feat: Added amberol, a simple GTK music player

---
+++ modules/user/programs/amberol/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  pkgs,
+  lib,
+}:
+let
+  cfg = config.userSettings.programs.amberol;
+in
+{
+  options.userSettings.programs.amberol.enable =
+    lib.mkEnableOption "Install amberol, a simple GTK audio player";
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      amberol
+    ];
+  };
+}
